f7e113916201ed7379d410681f9156da2e2d9e87
Artur Vazigin <artur.vazigin@quantumsimulations.de>
2024-12-12 09:03:16 +0100

    style: update theme colours to match evergarden theme

--- src/themes.sh
+++ src/themes.sh
@@ -4,31 +4,55 @@ SELECTED_THEME="$(tmux show-option -gv @tokyo-night-tmux_theme)"
 TRANSPARENT_THEME="$(tmux show-option -gv @tokyo-night-tmux_transparent)"
 
 case $SELECTED_THEME in
-"storm")
+"base")
   declare -A THEME=(
-    ["background"]="#24283b"
-    ["foreground"]="#a9b1d6"
-    ["black"]="#414868"
-    ["blue"]="#7aa2f7"
-    ["cyan"]="#7dcfff"
-    ["green"]="#73daca"
-    ["magenta"]="#bb9af7"
-    ["red"]="#f7768e"
-    ["white"]="#a9b1d6"
-    ["yellow"]="#e0af68"
+    ["background"]="#232A2E"
+    ["foreground"]="#D5D6C8"
+    ["black"]="#232A2E"
+    ["blue"]="#9BB5CF"
+    ["cyan"]="#93C9A1"
+    ["green"]="#B2C98F"
+    ["magenta"]="#E3A8D1"
+    ["red"]="#E67E80"
+    ["white"]="#D5D6C8"
+    ["yellow"]="#DBBC7F"
 
-    ["bblack"]="#414868"
-    ["bblue"]="#7aa2f7"
-    ["bcyan"]="#7dcfff"
-    ["bgreen"]="#41a6b5"
-    ["bmagenta"]="#bb9af7"
-    ["bred"]="#f7768e"
-    ["bwhite"]="#787c99"
-    ["byellow"]="#e0af68"
+    ["bblack"]="#313C40"
+    ["bblue"]="#6E8585"
+    ["bcyan"]="#5E6C70"
+    ["bgreen"]="#3F4D52"
+    ["bmagenta"]="#CCD4C1"
+    ["bred"]="#E69875"
+    ["bwhite"]="#94AAA0"
+    ["byellow"]="#46565B"
   )
   ;;
 
-"day")
+"soft")
+  declare -A THEME=(
+    ["background"]="#d5d6db"
+    ["foreground"]="#343b58"
+    ["black"]="#0f0f14"
+    ["blue"]="#34548a"
+    ["cyan"]="#0f4b6e"
+    ["green"]="#33635c"
+    ["magenta"]="#5a4a78"
+    ["red"]="#8c4351"
+    ["white"]="#343b58"
+    ["yellow"]="#8f5e15"
+
+    ["bblack"]="#9699a3"
+    ["bblue"]="#34548a"
+    ["bcyan"]="#0f4b6e"
+    ["bgreen"]="#33635c"
+    ["bmagenta"]="#5a4a78"
+    ["bred"]="#8c4351"
+    ["bwhite"]="#343b58"
+    ["byellow"]="#8f5815"
+  )
+  ;;
+
+"hard")
   declare -A THEME=(
     ["background"]="#d5d6db"
     ["foreground"]="#343b58"
